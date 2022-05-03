5f261de1ea32bfd853bcb7ad55e86551b1429e05
ChangSoon Lee <lcs07@hanmail.net>
2022-05-03 14:04:00 +0900

    코드 정리

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import Header from './Header'
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
 
-const App = ({ children }) => (
+const App = ({children}) => (
   <main>
     <Header />
     {children}
+    <Footer />
   </main>
-)
+);
 
-export default App
+export default App;
